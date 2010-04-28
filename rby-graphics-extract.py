824ace4efd1e4a0fa1fc3cdd1a0d68d65685d4ae
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2010-04-28 14:00:36 -0700

    Decompression seems to work.
    Just gotta figure out how the image data is stored.

--- rby-graphics-extract.py
+++ rby-graphics-extract.py
@@ -10,7 +10,7 @@ def bitflip(x, n):
         n -= 1
     return r
 
-table1 = [(1 << i) - 1 for i in range(16)]
+table1 = [(2 << i) - 1 for i in range(16)]
 
 #table2 = [
 #    [0x01, 0x32, 0x76, 0x45, 0xfe, 0xcd, 0x89, 0xba],
@@ -19,8 +19,8 @@ table1 = [(1 << i) - 1 for i in range(16)]
 #    [0xf7, 0x3b, 0x19, 0xd5, 0x08, 0xc4, 0xe6, 0x2a],
 #]
 table2 = [
-    [1, 0, 2, 3, 6, 7, 5, 4, 0xe, 0xf, 0xd, 0xc, 0x9, 0x8, 0xa, 0xb],
-    [0xf, 0xe, 0xc, 0xd, 0x8, 0x9, 0xb, 0xa, 0, 1, 3, 2, 7, 6, 4, 5],
+    [0, 1, 3, 2, 7, 6, 4, 5, 0xf, 0xe, 0xc, 0xd, 8, 9, 0xb, 0xa],
+    [0xf, 0xe, 0xc, 0xd, 8, 9, 0xb, 0xa, 0, 1, 3, 2, 7, 6, 4, 5],
 ]
 table2_mirrored = [
     [bitflip(x, 4) for x in table2[0]],
@@ -59,43 +59,29 @@ def decompress(f):
     sizey = readint(bs, 4)
     ramorder = next(bs)
 
-    mode = ['rle', 'data'][next(bs)]
-    mode2 = 0
-    while len(ram) < 0x188 * 2 * 4:
-            if mode == 'rle':
-                rle(ram, bs)
-                mode = 'data'
-            elif mode == 'data':
-                data(ram, bs)
-                mode = 'rle'
-            else:
-                assert False
-            print(hex(len(ram)), hex(0x188*4), hex(0x188 * 8))
-    
-    while len(ram) < 0x188 * 8:
-        ram.append(0)
+    ram1 = []
+    ram2 = []
+    rams = [ram1, ram2]
+    fillram(rams[ramorder], bs)
+    mode2 = next(bs)
+    if mode2 == 1:
+        mode2 = 1 + next(bs)
+    fillram(rams[ramorder ^ 1], bs)
 
-    ram = bitgroups_to_bytes(ram)
-    ram = bytearray(ram)
-
-    if ramorder == 0:
-        ram1 = ram[:0x188]
-        ram2 = ram[0x188:]
-    elif ramorder == 1:
-        ram1 = ram[0x188:]
-        ram2 = ram[:0x188]
-    else:
-        assert False
+    ram1 = bitgroups_to_bytes(ram1)
+    ram1 = bytearray(ram1)
+    ram2 = bitgroups_to_bytes(ram2)
+    ram2 = bytearray(ram2)
 
     if mode2 == 0:
         thing1(ram1)
         thing1(ram2)
     elif mode2 == 1:
-        thing1(ram1)
+        #thing1(ram1)
         thing2(ram1, ram2)
     elif mode2 == 2:
-        thing1(ram2)
-        thing2(ram1, ram2)
+        thing1(rams[ramorder^1])
+        thing2(rams[ramorder], rams[ramorder ^ 1])
 
     out = bytearray()
     for a, b in zip(ram1, ram2):
@@ -103,17 +89,25 @@ def decompress(f):
         out.append(b)
     return bytes(out)
 
-def checkram(ram, bs):
-    global mode2
-    if len(ram) == sizex * sizey * 4:
-        print("hi")
-        mode2 = next(bs)
-        if mode2 == 1:
-            mode2 = 1 + next(bs)
+def fillram(ram, bs):
+    size = sizex*sizey * 4
+    mode = ['rle', 'data'][next(bs)]
+    while len(ram) < size:
+        if mode == 'rle':
+            rle(ram, bs)
+            mode = 'data'
+        elif mode == 'data':
+            data(ram, bs)
+            mode = 'rle'
+        else:
+            assert False
+        #print(hex(len(ram)))
+        if len(ram) > size:
+            raise ValueError(size, len(ram))
 
 def bitgroups_to_bytes(bits):
     l = []
-    for i in range(0, len(bits)-1, 4):
+    for i in range(0, len(bits)-3, 4):
         n = ((bits[i] << 6)
              | (bits[i+1] << 4)
              | (bits[i+2] << 2)
@@ -126,39 +120,40 @@ def bitstream(f):
         char = f.read(1)
         if not char:
             break
-        byte = bitflip(char[0], 8)
+        byte = char[0]
 
-        i = 8
-        while 1:
-            yield byte & 1
-            i -= 1
-            if not i: break
-            byte >>= 1
+        yield (byte >> 7) & 1
+        yield (byte >> 6) & 1
+        yield (byte >> 5) & 1
+        yield (byte >> 4) & 1
+        yield (byte >> 3) & 1
+        yield (byte >> 2) & 1
+        yield (byte >> 1) & 1
+        yield byte & 1
 
-def readint(bs, n):
-    i = 0
-    while n:
-        i <<= 1
-        i |= next(bs)
-        n -= 1
-    return i
+def readint(bs, count):
+    n = 0
+    while count:
+        n <<= 1
+        n |= next(bs)
+        count -= 1
+    return n
 
 # the rle segment encodes chunks of zeros
 def rle(ram, bs):
     # count bits until we find a 0
     i = 0
-    for bit in bs:
-        if not bit:
-            break
+    while next(bs):
         i += 1
     
     n = table1[i]
+    a = readint(bs, i+1)
+    n += a
 
-    n += readint(bs, i+1)
+    #print(i, a, n)
 
     for i in range(n):
         ram.append(0)
-        #checkram(ram, bs)
 
 # data encodes pairs of bits
 def data(ram, bs):
@@ -168,22 +163,22 @@ def data(ram, bs):
         if bitgroup == 0:
             break
         ram.append(bitgroup)
-    #checkram(ram, bs)
+        #print("d: {:02b}".format(bitgroup))
 
 def thing1(ram, mirror=False):
-    prev = 0
     for x in range(sizex):
+        prev = 0
         for y in range(sizey):
             i = y*sizex + x
             a = ram[i] >> 4
             b = ram[i] & 0xf
             table = table2 if not mirror else table2_mirrored
 
-            bit = prev & 1 if not mirror else prev & 8
+            bit = bool(prev & 1 if not mirror else prev & 8)
             a = table[bit][a]
             prev = a
 
-            bit = prev & 1 if not mirror else prev & 8
+            bit = bool(prev & 1 if not mirror else prev & 8)
             b = table[bit][b]
             prev = b
 
@@ -200,10 +195,16 @@ def thing2(ram1, ram2, mirror=False):
                 pass
             ram1[i] ^= ram2[i]
 
-f = open("../red.gb", 'rb')
+f = open("../../red.gb", 'rb')
 f.seek(0x34000)
 out = decompress(f)
 from binascii import hexlify
-for i in range(sizey):
-    print(hexlify(out[i*sizex:(i+1)*sizex]))
+#for i in range(sizey):
+    #for j in range(4):
+        #row = b''.join(out[i*sizex + j*4 + k*16:i*sizex + j*4 + k*16 + 4] for k in range(4))
+        #print(hexlify(row))
+sizex //= 8
+for i in range(sizey*4):
+    row = out[i*sizex*4:(i+1)*sizex*4]
+    print(hexlify(row))
 
