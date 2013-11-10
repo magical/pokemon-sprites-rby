89301e4448e314e16c32fd9704bd1de5a8599c3c
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2013-11-09 23:00:43 -0800

    Fun With Palettes, and other stories.

--- rby-sprite-extract.go
+++ rby-sprite-extract.go
@@ -247,10 +247,14 @@ var (
 
 type ripper struct {
 	f *os.File
+	version string
 	spritePos [151]struct{
 		front int64
 		back int64
 	}
+	spritePalette [151]byte
+	sgbPalettes []color.Palette
+	cgbPalettes []color.Palette
 }
 
 func newRipper(f *os.File) (*ripper, error) {
@@ -264,11 +268,27 @@ func newRipper(f *os.File) (*ripper, error) {
 
 	header := rom[:0x150]
 	title := string(bytes.TrimRight(header[0x134:0x143], "\x00"))
+	hasCGB := rom[0x143] == 0x80
+	hasSGB := rom[0x146] == 3
 	isJP := rom[0x14A] == 0
+
+	_ = hasCGB
+	_ = hasSGB
 
 	getBank := getBankRBY
 	if isJP && (title == "POKEMON RED" || title == "POKEMON GREEN") {
 		getBank = getBankRG
+	}
+
+	switch title {
+	case "POKEMON RED":
+		z.version = "red"
+	case "POKEMON GREEN":
+		z.version = "green"
+	case "POKEMON BLUE":
+		z.version = "blue"
+	case "POKEMON YELLOW":
+		z.version = "yellow"
 	}
 
 	// Read pokedex order
@@ -279,7 +299,9 @@ func newRipper(f *os.File) (*ripper, error) {
 
 	internalId := make(map[int]int)
 	for i, n := range rom[pos : pos+0xbe] {
-		internalId[int(n)] = i + 1
+		if n != 0 {
+			internalId[int(n)] = i + 1
+		}
 	}
 
 	// Read sprite pointers
@@ -332,15 +354,53 @@ func newRipper(f *os.File) (*ripper, error) {
 	}
 
 	// Read palettes
-	//
+	pos = bytes.Index(rom, paletteMapBytes)
+	if pos < 0 {
+		return nil, fmt.Errorf("Couldn't find palettes")
+	}
+	paletteMap := rom[pos+1 : pos+152]
+	copy(z.spritePalette[:], paletteMap)
+
+	var palettes [40][4]colorRGB15
+	err = binary.Read(bytes.NewReader(rom[pos+152:]), binary.LittleEndian, &palettes)
+	if err != nil {
+		return nil, err
+	}
+	for _, p := range palettes {
+		var cp [4]color.Color
+		for i, c := range p {
+			cp[i] = color.Color(c.NRGBA())
+		}
+		z.sgbPalettes = append(z.sgbPalettes, color.Palette(cp[:]))
+	}
 
 	return z, nil
+}
+
+type colorRGB15 uint16
+
+func (rgb colorRGB15) NRGBA() color.NRGBA {
+	return color.NRGBA{
+		uint8(((rgb>>0&31)*255 + 15) / 31),
+		uint8(((rgb>>5&31)*255 + 15) / 31),
+		uint8(((rgb>>10&31)*255 + 15) / 31),
+		255,
+	}
+}
+
+func (rgb colorRGB15) RGBA() (r, g, b, a uint32) {
+	return rgb.NRGBA().RGBA()
 }
 
 func (z *ripper) Sprite(n int) (image.Image, error) {
 	ptr := z.spritePos[n-1].front
 	z.f.Seek(ptr, 0)
 	return Decode(z.f)
+}
+
+func (z *ripper) SpritePalette(n int, scheme string) (color.Palette) {
+	pi := z.spritePalette[n-1]
+	return z.sgbPalettes[pi]
 }
 
 // GetBankRG returns the bank containg the graphics for pokemon n.
@@ -388,6 +448,93 @@ var gameboyPalette = color.Palette{
 	color.NRGBA{0x0F, 0x28, 0x0F, 0xFF},
 }
 
+var gbBrownPalette = color.Palette{
+	color.NRGBA{0xFF, 0xFF, 0xFF, 0xFF},
+	color.NRGBA{0xFF, 173, 99, 0xFF},
+	color.NRGBA{132, 49, 0, 0xFF},
+	color.NRGBA{0, 0, 0, 0xFF},
+}
+
+var gbDarkBrownPalette = color.Palette{
+	color.NRGBA{R:0xff, G:0xe6, B:0xc5, A:0xff},
+	color.NRGBA{R:0xce, G:0x9c, B:0x84, A:0xff},
+	color.NRGBA{R:0x84, G:0x6b, B:0x29, A:0xff},
+	color.NRGBA{R:0x5a, G:0x31, B:0x8, A:0xff},
+}
+
+var gbPokemonRedPalette = color.Palette{
+	color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff},
+	color.NRGBA{R:0xff, G:0x84, B:0x84, A:0xff},
+	color.NRGBA{R:0x94, G:0x3a, B:0x3a, A:0xff},
+	color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff},
+}
+
+        //{color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff}, color.NRGBA{R:0x7b, G:0xff, B:0x31, A:0xff}, color.NRGBA{R:0x0, G:0x84, B:0x0, A:0xff}, color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff}},
+        //{color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff}, color.NRGBA{R:0xff, G:0x84, B:0x84, A:0xff}, color.NRGBA{R:0x94, G:0x3a, B:0x3a, A:0xff}, color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff}},
+
+var gbPokemonGreenPalette = color.Palette{
+	color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff},
+	color.NRGBA{R:0x7b, G:0xff, B:0x31, A:0xff},
+	color.NRGBA{R:0x0, G:0x63, B:0xc5, A:0xff},
+	color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff},
+}
+        //{color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff}, color.NRGBA{R:0xff, G:0x84, B:0x84, A:0xff}, color.NRGBA{R:0x94, G:0x3a, B:0x3a, A:0xff}, color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff}},
+        //{color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff}, color.NRGBA{R:0x7b, G:0xff, B:0x31, A:0xff}, color.NRGBA{R:0x0, G:0x63, B:0xc5, A:0xff}, color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff}},
+
+var gbPokemonBluePalette = color.Palette{
+	color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff},
+	color.NRGBA{R:0x63, G:0xa5, B:0xff, A:0xff},
+	color.NRGBA{R:0x0, G:0x0, B:0xff, A:0xff},
+	color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff},
+}
+        //{color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff}, color.NRGBA{R:0xff, G:0x84, B:0x84, A:0xff}, color.NRGBA{R:0x94, G:0x3a, B:0x3a, A:0xff}, color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff}},
+        //{color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff}, color.NRGBA{R:0x63, G:0xa5, B:0xff, A:0xff}, color.NRGBA{R:0x0, G:0x0, B:0xff, A:0xff}, color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff}},
+
+var gbPokemonYellowPalette = color.Palette{
+	color.NRGBA{R:0xff, G:0xff, B:0xff, A:0xff},
+	color.NRGBA{R:0xff, G:0xff, B:0x0, A:0xff},
+	color.NRGBA{R:0xff, G:0x0, B:0x0, A:0xff},
+	color.NRGBA{R:0x0, G:0x0, B:0x0, A:0xff},
+}
+
+func getValue(min, max int, v uint8) int {
+	u := float32(v)/255
+	return min + int(float32(max - min) * (2*u - u*u))
+}
+
+func muteColors(p color.Palette) {
+	for i := range p {
+		c := p[i].(color.NRGBA)
+		r := getValue(
+			getValue(33, 115, c.G),
+			getValue(198, 239, c.G),
+			c.R) - 33
+		r41 := getValue(0, 41, c.R)
+		r25 := getValue(0, 25, c.R)
+		r8 := getValue(0, 8, c.R)
+		g := getValue(
+			getValue(33+r41, 115+r25, c.B),
+			getValue(198+r25, 229+r8, c.B),
+			c.G) - 33
+		b := getValue(
+			getValue(33+r41, 115+r25, c.G),
+			getValue(198+r25, 229+r8, c.G),
+			c.B) - 33
+		p[i] = color.NRGBA{uint8(r), uint8(g), uint8(b), c.A}
+	}
+}
+
+func muteColors2(p color.Palette) {
+	for i := range p {
+		c := p[i].(color.NRGBA)
+		r, g, b := uint16(c.R), uint16(c.G), uint16(c.B)
+		rr := (r*195 + g*25 + b*0)>>8 + 35
+		gg := (r*25 + g*170 + b*25)>>8 + 35
+		bb := (r*25 + g*60 + b*125)>>8 + 40
+		p[i] = color.NRGBA{uint8(rr), uint8(gg), uint8(bb), c.A}
+	}
+}
+
 func main() {
 	flag.Parse()
 	filename := flag.Arg(0)
@@ -404,7 +551,7 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
-	montage(z, os.Stdout)
+	montage(z, os.Stdout, gbPokemonRedPalette)
 	/*
 	m, err := z.Sprite(1)
 	if err != nil {
@@ -417,17 +564,30 @@ func main() {
 	*/
 }
 
-func montage(z *ripper, w io.Writer) {
-	m := image.NewPaletted(image.Rect(0, 0, 56*15, 56*(151+14)/15), gameboyPalette)
+func montage(z *ripper, w io.Writer, pal color.Palette) {
+	b := image.Rect(0, 0, 56*15, 56*((151+14)/15))
+	var m draw.Image
+	if pal != nil {
+		pal = append(color.Palette(nil), pal...)
+		muteColors2(pal)
+		m = image.NewPaletted(b, pal)
+	} else {
+		m = image.NewNRGBA(b)
+		bg := image.NewUniform(z.SpritePalette(1, "sgb")[0])
+		draw.Draw(m, m.Bounds(), bg, image.ZP, draw.Src)
+	}
 	tile := image.Rect(0, 0, 56, 56)
-	//draw.Draw(m, m.Bounds(), image.White, image.ZP, draw.Src)
 	for i := 0; i < 151; i++ {
 		spr, err := z.Sprite(i+1)
 		if err != nil {
-			log.Printf("error getting pokemon %d: %v", i, err)
+			log.Printf("error getting pokemon %d: %v", i+1, err)
 		}
 		p := spr.(*image.Paletted)
-		p.Palette = gameboyPalette
+		if pal != nil {
+			p.Palette = pal
+		} else {
+			p.Palette = z.SpritePalette(i+1, "sgb")
+		}
 		padding := tile.Size().Sub(p.Rect.Size()).Div(2)
 		p.Rect = p.Rect.Add(padding)
 		row := i / 15
