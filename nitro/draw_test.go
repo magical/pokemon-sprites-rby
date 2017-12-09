c616e85ec3b6045679e15db4aa54009b88946b25
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2017-12-08 19:05:00 -0800

    Nitro: add rotate test
    Add a test for rotate, and refactor the test for drawPalettedUnder.
    
    Uncommitted change from a while ago.

--- nitro/draw_test.go
+++ nitro/draw_test.go
@@ -4,36 +4,82 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
-	"math/rand"
 	"testing"
 )
+
+func checkerboard(pal color.Palette) *image.Paletted {
+	p := image.NewPaletted(image.Rect(0, 0, 16, 16), pal)
+	for y := 0; y < 16; y++ {
+		for x := 0; x < 16; x++ {
+			p.SetColorIndex(x, y, uint8((x+y)%2))
+		}
+	}
+	return p
+}
+
+func uniform(index uint8, pal color.Palette) *image.Paletted {
+	p := image.NewPaletted(image.Rect(0, 0, 16, 16), pal)
+	for y := 0; y < 16; y++ {
+		for x := 0; x < 16; x++ {
+			p.SetColorIndex(x, y, index)
+		}
+	}
+	return p
+}
 
 func TestDrawPalettedUnder(t *testing.T) {
 	// Test transparency
 	pal := color.Palette{
 		color.Transparent,
-		color.White,
 		color.Black,
+		color.White,
 	}
-	r := image.Rect(0, 0, 100, 100)
+	r := image.Rect(0, 0, 16, 16)
+	dst := checkerboard(pal)
+	src := uniform(2, pal)
 	rgb := image.NewRGBA(r)
-	dst := image.NewPaletted(r, pal)
-	src := image.NewPaletted(r, pal)
-	for i := range dst.Pix {
-		dst.Pix[i] = uint8(rand.Intn(len(pal)))
-	}
-	for i := range src.Pix {
-		src.Pix[i] = uint8(rand.Intn(len(pal)))
-	}
-	r1 := r
-	//r1 := image.Rect(45, 45, 55, 55)
-	draw.Draw(rgb, r1, src, r1.Min, draw.Src)
+	draw.Draw(rgb, r, src, r.Min, draw.Src)
 	draw.Draw(rgb, r, dst, r.Min, draw.Over)
-	drawPalettedUnder(dst, r1, src, r1.Min)
+	drawPalettedUnder(dst, r, src, r.Min)
 	for x := 0; x < 100; x++ {
 		for y := 0; y < 100; y++ {
 			if !equal(dst.At(x, y), rgb.At(x, y)) {
 				t.Errorf("color at %+v, %+v: got %v, expected %v", x, y, dst.At(x, y), rgb.At(x, y))
+			}
+		}
+	}
+}
+
+func diag() image.Image {
+	m := image.NewRGBA(image.Rect(0, 0, 16, 16))
+	for y := 0; y < 16; y++ {
+	for x := 0; x < 16; x++ {
+		m.SetRGBA(x, y, color.RGBA{uint8(((x+y)*255 + 15 )/30), 0, 0, 255})
+	}
+	}
+	return m
+}
+
+func TestRotate(t *testing.T) {
+	// Rotating by 0 should be a no-op.
+	dst := image.NewRGBA(image.Rect(0, 0, 16, 16))
+	src := diag()
+	rotate(dst, dst.Bounds(), image.ZP, src, image.ZP, 1, 1, 0)
+	for y := 0; y < 16; y++ {
+	for x := 0; x < 16; x++ {
+		if !equal(dst.At(x, y), src.At(x,y)) {
+			t.Errorf("color at (%v,%v): got %v, want %v", x, y, dst.At(x, y), src.At(x, y))
+		}
+	}
+	}
+
+	// Rotating by 45°
+	dst = image.NewRGBA(image.Rect(0, 0, 16, 16))
+	rotate(dst, dst.Bounds(), image.Pt(15, 0), src, image.ZP, 1, 1, .25)
+	for y := 0; y < 16; y++ {
+		for x := 0; x < 16; x++ {
+			if !equal(dst.At(x, y), src.At(y, 15-x)) {
+				t.Errorf("color at (%v,%v): got %v, want %v", x, y ,dst.At(x, y), src.At(y, 15-x))
 			}
 		}
 	}
